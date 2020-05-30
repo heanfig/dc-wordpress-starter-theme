df8b22ced3e2a8c66ed5d6cb56cf28d6fc6d24d7
Herman Andres <heanfig@gmail.com>
2020-05-29 22:11:04 -0500

    [CORE] Add defaults TGM

--- functions.php
+++ functions.php
@@ -181,3 +181,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Add TGM activation
+ */
+require get_template_directory() . '/inc/dc-core.php';
+
